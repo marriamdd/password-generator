06edebd4392a5987c1ef7ce82ba1130caf37023d
marriamdd <davitashvili.m3@gmail.com>
2024-04-03 21:04:08 +0400

    add button for generate after click add some styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useState, ChangeEvent } from "react";
-
+import styled from "styled-components";
 import "./App.css";
 
 const symbols = [
@@ -103,46 +103,52 @@ function App() {
   const handleUpperCase = (event: ChangeEvent<HTMLInputElement>) => {
     if (event.target.checked) {
       passwordArray.push(uppercaseChars);
-      generatePassword();
     }
   };
   const handleLowerCase = (event: ChangeEvent<HTMLInputElement>) => {
     if (event.target.checked) {
       passwordArray.push(lowercaseChars);
-      generatePassword();
     }
   };
   const handleNumber = (event: ChangeEvent<HTMLInputElement>) => {
-    passwordArray.push(numbers);
-    generatePassword();
+    if (event.target.checked) {
+      passwordArray.push(numbers);
+    }
   };
   const handleSymbol = (event: ChangeEvent<HTMLInputElement>) => {
     if (event.target.checked) {
       passwordArray.push(symbols);
-      generatePassword();
     }
   };
 
   const generatePassword = () => {
     let newPassword = "";
     let handleNestedPassword = passwordArray.flat();
-    for (let i = 0; i < range; i++) {
-      const randomIndex = Math.floor(
-        Math.random() * handleNestedPassword.length
-      );
+    if (passwordArray.length > 0) {
+      for (let i = 0; i < range; i++) {
+        const randomIndex = Math.floor(
+          Math.random() * handleNestedPassword.length
+        );
 
-      newPassword += handleNestedPassword[randomIndex];
+        newPassword += handleNestedPassword[randomIndex];
+      }
+
+      setPassword(newPassword);
     }
-
-    setPassword(newPassword);
   };
+
   return (
     <>
-      <div>
+      <PasswordGraph>
         <p>{password}</p>
+      </PasswordGraph>
+      <Main>
         <div>
-          <div>
-            <p>{range}</p>
+          <ChangeLength>
+            <p>Character Length</p>
+            <span>{range}</span>
+          </ChangeLength>
+          <RangeDiv>
             <input
               type="range"
               min="0"
@@ -151,44 +157,120 @@ function App() {
               value={range}
               onChange={handleRangeChange}
             />
-          </div>
-          <div
-            style={{
-              display: "flex",
-              flexDirection: "column",
-              justifyContent: "center",
-            }}
-          >
-            <div>
-              <label htmlFor="checkBox1">include uppercase</label>
+          </RangeDiv>
+
+          <GeneratorContainer>
+            <CheckBoxDiv>
               <input
                 onChange={handleUpperCase}
                 type="checkBox"
                 id="checkBox1"
               />
-            </div>
-            <div>
-              <label htmlFor="checkBox2">include lowercase</label>
+              <label htmlFor="checkBox1">Include Uppercase Letters</label>
+            </CheckBoxDiv>
+            <CheckBoxDiv>
               <input
                 type="checkBox"
                 id="checkBox2"
                 onChange={handleLowerCase}
               />
-            </div>
-            <div>
-              <label htmlFor="checkBox3">include numbers</label>
-
+              <label htmlFor="checkBox2">Include Lowercase Letters</label>
+            </CheckBoxDiv>
+            <CheckBoxDiv>
               <input type="checkBox" id="checkBox3" onChange={handleNumber} />
-            </div>
-            <div>
-              <label htmlFor="checkBox4">include symbols</label>
+              <label htmlFor="checkBox3">Include Numbers</label>
+            </CheckBoxDiv>
+            <CheckBoxDiv>
               <input type="checkBox" id="checkBox4" onChange={handleSymbol} />
-            </div>
-          </div>
+              <label htmlFor="checkBox4">Include Symbols</label>
+            </CheckBoxDiv>
+          </GeneratorContainer>
         </div>
-      </div>
+        <Button onClick={generatePassword}>GENERATE</Button>
+      </Main>
     </>
   );
 }
 
+const Main = styled.div`
+  background: var(--Dark-Grey, #24232c);
+  width: 343px;
+  height: 423px;
+`;
+const PasswordGraph = styled.div`
+  background: var(--Dark-Grey, #24232c);
+  width: 343px;
+  height: 64px;
+  color: var(--Almost-White, #e6e5ea);
+  margin-bottom: 2rem;
+  font-family: "JetBrains Mono";
+  font-size: 24px;
+  display: flex;
+  align-items: center;
+  font-weight: 700;
+  line-height: normal;
+  & > p {
+    padding: 1rem;
+  }
+`;
+const CheckBoxDiv = styled.div`
+  color: var(--Almost-White, #e6e5ea);
+  font-family: "JetBrains Mono";
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+  padding: 1rem 2rem 0rem 2rem;
+`;
+
+const GeneratorContainer = styled.div``;
+const ChangeLength = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 1rem 3rem 1rem 3rem;
+  & > p {
+    color: var(--Almost-White, #e6e5ea);
+    font-family: "JetBrains Mono";
+    font-size: 16px;
+    font-style: normal;
+    font-weight: 700;
+    line-height: normal;
+  }
+  & > span {
+    color: var(--Neon-Green, #a4ffaf);
+    font-family: "JetBrains Mono";
+    font-size: 24px;
+    font-style: normal;
+    font-weight: 700;
+    line-height: normal;
+  }
+`;
+const RangeDiv = styled.div`
+  width: 100%;
+  height: 20px;
+  flex-shrink: 0;
+  display: flex;
+
+  justify-content: center;
+  & > input {
+    width: 311px;
+    height: 8px;
+  }
+`;
+const Button = styled.button`
+  width: 311px;
+  height: 56px;
+  flex-shrink: 0;
+  background: var(--Neon-Green, #a4ffaf);
+  margin-top: 90px;
+  margin-left: 15px;
+  color: var(--Dark-Grey, #24232c);
+  text-align: center;
+  font-family: "JetBrains Mono";
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+`;
 export default App;
