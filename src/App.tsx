e52d726696382653807253af7aebb7a7f18e79ad
marriamdd <davitashvili.m3@gmail.com>
2024-04-05 00:41:26 +0400

    remake generator functional shorter

--- src/App.tsx
+++ src/App.tsx
@@ -1,103 +1,105 @@
 import { useState, ChangeEvent } from "react";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import "./App.css";
-
-const symbols = [
-  "!",
-  "@",
-  "#",
-  "$",
-  "%",
-  "^",
-  "&",
-  "*",
-  "(",
-  ")",
-  "_",
-  "+",
-  "-",
-  "=",
-  "{",
-  "}",
-  "[",
-  "]",
-  "|",
-  ":",
-  ";",
-  '"',
-  "'",
-  "<",
-  ">",
-  ",",
-  ".",
-  "/",
-  "?",
+const allType = [
+  [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+  ],
+  [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+  ],
+  ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"],
+  [
+    "!",
+    "@",
+    "#",
+    "$",
+    "%",
+    "^",
+    "&",
+    "*",
+    "(",
+    ")",
+    "_",
+    "+",
+    "-",
+    "=",
+    "{",
+    "}",
+    "[",
+    "]",
+    "|",
+    ":",
+    ";",
+    '"',
+    "'",
+    "<",
+    ">",
+    ",",
+    ".",
+    "/",
+    "?",
+  ],
 ];
 
-const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+let passwordArray: string[] = [];
 
-const lowercaseChars = [
-  "a",
-  "b",
-  "c",
-  "d",
-  "e",
-  "f",
-  "g",
-  "h",
-  "i",
-  "j",
-  "k",
-  "l",
-  "m",
-  "n",
-  "o",
-  "p",
-  "q",
-  "r",
-  "s",
-  "t",
-  "u",
-  "v",
-  "w",
-  "x",
-  "y",
-  "z",
-];
-
-const uppercaseChars = [
-  "A",
-  "B",
-  "C",
-  "D",
-  "E",
-  "F",
-  "G",
-  "H",
-  "I",
-  "J",
-  "K",
-  "L",
-  "M",
-  "N",
-  "O",
-  "P",
-  "Q",
-  "R",
-  "S",
-  "T",
-  "U",
-  "V",
-  "W",
-  "X",
-  "Y",
-  "Z",
-];
-let passwordArray: any = [];
 function App() {
   const [gradient, setGradient] = useState(50);
   const [range, setRange] = useState<string>("8");
   const [password, setPassword] = useState<string>("");
+  const [background, setBackground] = useState<number | null>(null);
+
   const handleRangeChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setRange(event.target.value);
     const value = parseInt(event.target.value);
@@ -106,27 +108,7 @@ function App() {
       100;
     setGradient(newMiddlePoint);
   };
-  const handleUpperCase = (event: ChangeEvent<HTMLInputElement>) => {
-    if (event.target.checked) {
-      passwordArray.push(uppercaseChars);
-    }
-  };
-  const handleLowerCase = (event: ChangeEvent<HTMLInputElement>) => {
-    if (event.target.checked) {
-      passwordArray.push(lowercaseChars);
-    }
-  };
-  const handleNumber = (event: ChangeEvent<HTMLInputElement>) => {
-    if (event.target.checked) {
-      passwordArray.push(numbers);
-    }
-  };
-  const handleSymbol = (event: ChangeEvent<HTMLInputElement>) => {
-    if (event.target.checked) {
-      passwordArray.push(symbols);
-    }
-  };
-
+  let generated: string[] = [];
   const generatePassword = () => {
     let newPassword = "";
     let handleNestedPassword = passwordArray.flat();
@@ -138,13 +120,37 @@ function App() {
 
         newPassword += handleNestedPassword[randomIndex];
       }
-      console.log(handleNestedPassword);
+
       setPassword(newPassword);
+    }
+    console.log(passwordArray);
+  };
+
+  const collectChosenType = (
+    event: React.ChangeEvent<HTMLInputElement>,
+    id: string
+  ) => {
+    if (event.target.checked) {
+      const collect = allType.map((item, index) => {
+        if (index === +id) {
+          passwordArray.push(...item);
+          console.log(passwordArray);
+        }
+      });
+
+      // for (let i = 0; i < +range; i++) {
+      //   const randomIndex = Math.floor(Math.random() * +passwordArray.length);
+      //   generated.push(passwordArray[randomIndex]);
+      //   console.log(passwordArray[randomIndex]);
+      // }
+
+      // generated.splice(0, passwordArray.length);
     }
   };
 
   return (
     <>
+      <Title>Password Generator</Title>
       <PasswordGraph>
         <p>{password}</p>
       </PasswordGraph>
@@ -172,30 +178,50 @@ function App() {
           <GeneratorContainer>
             <CheckBoxDiv>
               <input
-                onChange={handleUpperCase}
                 type="checkBox"
-                id="checkBox1"
+                id="1"
+                onChange={() => collectChosenType(event, "0")}
               />
-              <label htmlFor="checkBox1">Include Uppercase Letters</label>
+              <label htmlFor="1">Include Uppercase Letters</label>
             </CheckBoxDiv>
             <CheckBoxDiv>
               <input
                 type="checkBox"
-                id="checkBox2"
-                onChange={handleLowerCase}
+                id="2"
+                onChange={() => collectChosenType(event, "1")}
               />
-              <label htmlFor="checkBox2">Include Lowercase Letters</label>
+              <label htmlFor="2">Include Lowercase Letters</label>
             </CheckBoxDiv>
             <CheckBoxDiv>
-              <input type="checkBox" id="checkBox3" onChange={handleNumber} />
-              <label htmlFor="checkBox3">Include Numbers</label>
+              <input
+                type="checkBox"
+                id="3"
+                onChange={() => collectChosenType(event, "2")}
+              />
+              <label htmlFor="3">Include Numbers</label>
             </CheckBoxDiv>
             <CheckBoxDiv>
-              <input type="checkBox" id="checkBox4" onChange={handleSymbol} />
-              <label htmlFor="checkBox4">Include Symbols</label>
+              <input
+                type="checkBox"
+                id="4"
+                onChange={() => collectChosenType(event, "3")}
+              />
+              <label htmlFor="4">Include Symbols</label>
             </CheckBoxDiv>
           </GeneratorContainer>
         </div>
+        <StrengthContainer $background={background}>
+          <div>
+            <h2>STRENGTH</h2>
+            <h3>MEDIUM</h3>
+          </div>
+          <div>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+        </StrengthContainer>
         <Button onClick={generatePassword}>GENERATE</Button>
       </Main>
     </>
@@ -204,8 +230,6 @@ function App() {
 
 const Main = styled.div`
   background: var(--Dark-Grey, #24232c);
-  width: 343px;
-  height: 423px;
 `;
 const PasswordGraph = styled.div`
   background: var(--Dark-Grey, #24232c);
@@ -233,7 +257,39 @@ const CheckBoxDiv = styled.div`
   padding: 1rem 2rem 0rem 2rem;
 `;
 
-const GeneratorContainer = styled.div``;
+const GeneratorContainer = styled.div`
+  & > div {
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+    input[type="checkbox"] {
+      appearance: none;
+      width: 20px;
+      height: 20px;
+      flex-shrink: 0;
+      border: 2px solid #ccc;
+      border-radius: 4px;
+      outline: none;
+      position: relative;
+      cursor: pointer;
+    }
+    input[type="checkbox"]::before {
+      content: "";
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      width: 10px;
+      height: 10px;
+      background: var(--Neon-Green, #a4ffaf);
+      border-radius: 2px;
+      display: none;
+    }
+    input[type="checkbox"]:checked::before {
+      display: block;
+    }
+  }
+`;
 const ChangeLength = styled.div`
   display: flex;
   align-items: center;
@@ -293,8 +349,7 @@ const Button = styled.button`
   height: 56px;
   flex-shrink: 0;
   background: var(--Neon-Green, #a4ffaf);
-  margin-top: 90px;
-  margin-left: 15px;
+  margin: 0rem 1rem 1rem;
 
   color: var(--Dark-Grey, #24232c);
   text-align: center;
@@ -303,5 +358,58 @@ const Button = styled.button`
   font-style: normal;
   font-weight: 700;
   cursor: pointer;
+`;
+
+const Title = styled.p`
+  color: #817d92;
+  text-align: center;
+
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+`;
+const StrengthContainer = styled.div`
+  width: 311px;
+  height: 56px;
+  margin: 1rem 1rem 1rem;
+  background: #18171f;
+  display: flex;
+
+  & > :nth-of-type(1) {
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+    padding: 1rem;
+    & > h3 {
+      color: var(--Almost-White, #e6e5ea);
+      text-align: right;
+
+      font-size: 18px;
+
+      font-weight: 700;
+    }
+    & > h2 {
+      color: var(--Grey, #817d92);
+      text-align: center;
+
+      font-size: 16px;
+
+      font-weight: 700;
+    }
+  }
+  & > :nth-of-type(2) {
+    display: flex;
+    gap: 1rem;
+    align-items: center;
+    margin-left: 1rem;
+
+    & > span {
+      width: 10px;
+      height: 28px;
+
+      background-color: #e6e5ea;
+    }
+  }
 `;
 export default App;
